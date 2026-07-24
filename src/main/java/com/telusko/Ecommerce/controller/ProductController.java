f49d38d604fc3b69f3ef0c5db7659b7c1e80e301
seeram17 <seeramkarthikeya@gmail.com>
2026-07-24 12:07:56 +0530

    Add ProductController - GET /api/products

---
+++ src/main/java/com/telusko/Ecommerce/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.telusko.Ecommerce.controller;
+
+
+import com.telusko.Ecommerce.model.Product;
+import com.telusko.Ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:5173")
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    ProductService service  ;
+
+    @RequestMapping("/")
+    public String greet(){ return "Hello World";}
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts(){
+        return new ResponseEntity<>(service.getProducts(), HttpStatus.OK);
+    }
+
+}
