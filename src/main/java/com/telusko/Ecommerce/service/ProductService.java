306172b15e62474eca851c156885270762e7eeb8
seeram17 <seeramkarthikeya@gmail.com>
2026-07-24 11:34:22 +0530

    ProductService - add getProductById() + addProduct()

--- src/main/java/com/telusko/Ecommerce/service/ProductService.java
+++ src/main/java/com/telusko/Ecommerce/service/ProductService.java
@@ -4,7 +4,9 @@ import com.telusko.Ecommerce.model.Product;
 import com.telusko.Ecommerce.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -14,4 +16,17 @@ public class ProductService {
     ProductRepository repo  ;
 
     public List<Product> getProducts(){ return repo.findAll();}
+
+    public Product getProductById(int id) {return repo.findById(id).orElse(null);}
+
+    public Product addProduct(Product product, MultipartFile multipartFile) throws IOException {
+
+        product.setImageName(multipartFile.getOriginalFilename());
+        product.setImageType(multipartFile.getContentType());
+        product.setImageData(multipartFile.getBytes());
+
+        repo.save(product) ;
+
+        return product ;
+    }
 }
